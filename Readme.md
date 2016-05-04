e74812966922e9679fb9a7419229c9a123b6b7f3
Josh Taylor <joshrtay@gmail.com>
2016-05-04 16:05:29 -0700

    FIRST

---
+++ Readme.md
@@ -0,0 +1,41 @@
+
+# terra-gateway
+
+[![Build status][travis-image]][travis-url]
+[![Git tag][git-image]][git-url]
+[![NPM version][npm-image]][npm-url]
+[![Code style][standard-image]][standard-url]
+
+Create terraform_api_gateway configs from a simple spec.
+
+## Installation
+
+    $ npm install terra-gateway
+
+## Usage
+
+```js
+var terraGateway = require('terra-gateway')
+
+```
+
+## API
+
+### terraGateway(arg)
+
+- `arg` -
+
+**Returns:**
+
+## License
+
+MIT
+
+[travis-image]: https://img.shields.io/travis/joshrtay/terra-gateway.svg?style=flat-square
+[travis-url]: https://travis-ci.org/joshrtay/terra-gateway
+[git-image]: https://img.shields.io/github/tag/joshrtay/terra-gateway.svg?style=flat-square
+[git-url]: https://github.com/joshrtay/terra-gateway
+[standard-image]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square
+[standard-url]: https://github.com/feross/standard
+[npm-image]: https://img.shields.io/npm/v/terra-gateway.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/terra-gateway
---
+++ test/index.js
@@ -0,0 +1,68 @@
+/**
+ * Imports
+ */
+
+const test = require('tape')
+const terraGateway = require('..')
+
+/**
+ * Tests
+ */
+
+test('should work', (t) => {
+
+  const spec = {
+    "id": "function_router",
+    "name": "Function Router API",
+    "description": "Route requests to functions.",
+    "methods": {
+      "GET": {
+        "request": {
+          "credentials": "${aws_iam_role.gateway_invoke_lambda.arn}",
+          "lambda_arn": "function_router",
+          "templates": {
+            "application/json": {
+              "body": "$input.json('$')",
+              "name": "$input.params('name')",
+              "url": "$url",
+              "headers": {
+                "Content-Type": "$input.params().get('Content-Type')",
+                "Host": "$input.params().header.get('Host')",
+                "cookie": "$input.params('cookie')",
+              },
+              "header-params": "$input.params().header"
+            }
+          }
+        },
+        "responses": {
+          "200": {
+            "models": {
+              "application/json": "Empty",
+              "text/html": "Empty"
+            },
+            "headers": {
+              "Content-Type": "integration.response.body.type"
+            },
+            "body": {
+              "application/json": "$input.path('$.body')"
+            }
+          }
+        }
+      }
+    },
+    "stage": "prod"
+  }
+
+  terraGateway.DEPTH = 2
+  terraGateway(spec, __dirname + '/function_router.tf.json')
+
+  const out = require('./function_router.tf.json')
+  t.ok(out.resource)
+  t.ok(out.resource["aws_api_gateway_rest_api"])
+  t.ok(out.resource["aws_api_gateway_resource"])
+  t.ok(out.resource["aws_api_gateway_method"])
+  t.ok(out.resource["aws_api_gateway_integration"])
+  t.ok(out.resource["aws_api_gateway_integration_response"])
+  t.ok(out.resource["aws_api_gateway_deployment"])
+  t.end()
+})
